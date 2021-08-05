47aaa4c17626aef046cdaeedc38fcb584c98cdfa
Yangxiaohan0120 <17853138921@163.com>
2021-08-05 10:44:50 +0800

    FastFeature

--- src/main/java/utils/FastqFeature.java
+++ src/main/java/utils/FastqFeature.java
@@ -18,18 +18,18 @@ public class FastqFeature {
     String[] phreds = {"!", "\"", "#", "$", "%", "&", " ", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I"};
     int[] score = new int[41];
     static HashMap<Integer, String> scoreQMap = new HashMap<>();
-    static HashMap<String,Integer> phredQMap = new HashMap<>();
+    static HashMap<String, Integer> phredQMap = new HashMap<>();
 
-    public FastqFeature(){
+    public FastqFeature() {
         this.getHashMap();
     }
 
-    public void getHashMap(){
+    public void getHashMap() {
         for (int i = 0; i < 41; i++) {
             scoreQMap.put(score[i], phreds[i]);
         }
         for (int i = 0; i < 41; i++) {
-            phredQMap.put(phreds[i],score[i]);
+            phredQMap.put(phreds[i], score[i]);
         }
     }
 
